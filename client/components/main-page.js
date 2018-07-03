7a95203eb350160aa1ba293314ec733bbc5348a1
jmunoz1992 <jasmine.esplago.munoz@gmail.com>
2018-07-03 12:53:18 -0500

    toggling working and next card function working

--- client/components/main-page.js
+++ client/components/main-page.js
@@ -8,30 +8,68 @@ import { Card } from 'semantic-ui-react'
  */
 class MainPage extends React.Component {
   constructor(props){
-    super(props)
+    super(props);
+    this.state = {
+      active: false,
+      count: 0,
+    }
   }
 
   componentDidMount() {
     this.props.getFlashcards();
   }
 
-    render () {
-      const { flashcards } = this.props;
-      console.log('flashcards ', flashcards);
-      return (
-        <div>
-          <h1>FLASHCARDS UP IN HURRRRR</h1>
-          {flashcards.map(flashcard =>
-              <Card key={flashcard.id}>
+
+  toggleDescriptionClick = () => {
+    if(!this.state.active) {
+      this.setState({active: true})
+    } else {
+      this.setState({active: false})
+    }
+  }
+
+  goToNextCardClick = () => {
+    let newCount = 0;
+    console.log('state count ', this.state.count);
+    console.log('length ', this.props.flashcards.length);
+    if(this.state.count < this.props.flashcards.length - 1) {
+      newCount = this.state.count + 1;
+    } else {
+      newCount = 0;
+    }
+    this.setState({count: newCount});
+  }
+
+  render () {
+    const { flashcards } = this.props;
+    const {count} = this.state;
+    const divStyle = {
+      top: '50%',
+      left: '50%',
+      width: '50%',
+      margin: '0 auto',
+      textAlign: 'center'
+    };
+    return (
+      <div style={divStyle}>
+        {flashcards.length && count < flashcards.length?
+          <div>
+            <h1>FLASHCARDS UP IN HURRRRR</h1>
+            <div style={divStyle}>
+              <Card key={flashcards[count].id} onClick={this.toggleDescriptionClick}>
                 <Card.Content>
-                  <Card.Header>{flashcard.question}</Card.Header>
-                  <Card.Description>{flashcard.answer}</Card.Description>
+                  <Card.Header>{flashcards[count].question}</Card.Header>
+                  {this.state.active ? <Card.Description>{flashcards[count].answer}</Card.Description> : null}
                 </Card.Content>
               </Card>
-            )}
-        </div>
-      )
-    }
+            </div>
+          </div>
+          : null
+        }
+        <button onClick={this.goToNextCardClick}>NEXT CARD</button>
+      </div>
+    )
+  }
 }
 
 /**
