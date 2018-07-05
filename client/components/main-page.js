0a7fe526763b8368bc7079e595d7732d6d4ef650
jmunoz1992 <jasmine.esplago.munoz@gmail.com>
2018-07-05 09:44:38 -0500

    confirmation logic refined

--- client/components/main-page.js
+++ client/components/main-page.js
@@ -21,7 +21,6 @@ class MainPage extends React.Component {
     this.props.getFlashcards();
   }
 
-
   toggleDescriptionClick = () => {
     if(!this.state.active) {
       this.setState({active: true})
@@ -58,7 +57,9 @@ class MainPage extends React.Component {
     evt.preventDefault();
     const userInput = this.state.input;
     const flashcardAnswer = this.props.flashcards[this.state.count].answer;
-    if(this.checkAnswers(userInput, flashcardAnswer)) {
+    if(userInput === "") {
+      this.setState({inputCheck: "Please Type In An Answer Below"})
+    } else if(this.checkAnswers(userInput, flashcardAnswer)) {
       this.setState({inputCheck: "You Got It!"});
     } else {
       this.setState({inputCheck: "Not Quite, Try Again!"});
@@ -71,10 +72,8 @@ class MainPage extends React.Component {
 
   render () {
     const { flashcards } = this.props;
-    const {count, inputCheck} = this.state;
+    const {count, inputCheck, input} = this.state;
     const divStyle = {
-      top: '50%',
-      left: '50%',
       width: '50%',
       margin: '0 auto',
       textAlign: 'center'
@@ -82,15 +81,10 @@ class MainPage extends React.Component {
     const cardStyle = {
       width: '50%',
       margin: 'auto auto',
-      textAlign: 'center'
     };
-    const buttonStyle = {
-      paddng: '50px',
-      margin: '10px',
-    }
     return (
       <div style={divStyle}>
-        {flashcards.length && count < flashcards.length?
+        {flashcards.length && count < flashcards.length ?
           <div>
             <h1>FLASHCARDS UP IN HURRRRR</h1>
             <br />
@@ -108,19 +102,20 @@ class MainPage extends React.Component {
             <div>
               {inputCheck.length ?
                 inputCheck === "You Got It!" ?
-                <Message positive>
-                  <Message.Header>You Got It!</Message.Header>
-                </Message> :
-                <Message negative>
-                  <Message.Header>Not Quite, Try Again!</Message.Header>
-                </Message>
+                  (<Message positive>
+                    <Message.Header>{inputCheck}</Message.Header>
+                  </Message>)
+                :
+                  (<Message negative>
+                    <Message.Header>{inputCheck}</Message.Header>
+                  </Message>)
               : null}
             </div>
             <br />
             <br />
             <div>
               <Form onSubmit={this.handleSubmit}>
-                <TextArea type="text" placeholder="Type in answer here" value={this.state.input} onChange={this.handleChange}/>
+                <TextArea type="text" placeholder="Type in answer here" value={input} onChange={this.handleChange}/>
                 <br />
                 <br />
                 <Button type="submit" content="Submit" color="green"/>
