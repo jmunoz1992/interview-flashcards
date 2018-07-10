a0f6cebb8b482f7d4fa01eb5b72a3c348e45e7a4
jmunoz1992 <jasmine.esplago.munoz@gmail.com>
2018-07-10 10:37:36 -0500

    tweaking POST test

--- server/api/flashcards.js
+++ server/api/flashcards.js
@@ -1,11 +1,31 @@
 const router = require('express').Router()
-const {Flashcard} = require('../db/models')
+const {Flashcard, Pack} = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
     const flashcards = await Flashcard.findAll()
     res.json(flashcards)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const createdFlashcard = await Flashcard.create({
+      question: req.body.question,
+      answer: req.body.answer,
+      type: req.body.type,
+    })
+    const type = req.body.type[0].toUpperCase() + req.body.type.slice(1);
+    const matchingPack = await Pack.findOne({
+      where: {
+        name: type
+      }
+    })
+    createdFlashcard.setPack(matchingPack.id)
+    res.status(201).json(createdFlashcard)
   } catch (err) {
     next(err)
   }
--- server/api/flashcards.spec.js
+++ server/api/flashcards.spec.js
@@ -5,6 +5,7 @@ const request = require('supertest')
 const db = require('../db')
 const app = require('../index')
 const Flashcard = db.model('flashcard')
+const agent = require('supertest')(app);
 
 describe('Flashcard routes', () => {
   beforeEach(() => {
@@ -29,6 +30,20 @@ describe('Flashcard routes', () => {
       expect(res.body[0].question).to.be.equal('What is the difference between .call and .apply?')
       expect(res.body[0].answer).to.be.equal('Both .call and .apply are used to invoke functions and the first parameter will be used as the value of this within the function. However, .call takes in comma-separated arguments as the next arguments while .apply takes in an array of arguments as the next argument. An easy way to remember this is C for call and comma-separated and A for apply and an array of arguments.')
       expect(res.body[0].type).to.be.equal('frontend')
+    })
+
+    it('POST /api/flashcards', async () => {
+      const response = await agent.post('/api/flashcards')
+      .send({
+        question: 'What is the store for in redux?',
+        answer: "An easier way to manage state",
+        type: "redux"
+      })
+      .expect(201);
+    const createdFlashcard = await Flashcard.findById(response.body.id);
+    expect(createdFlashcard.question).to.be.equal('What is the store for in redux?');
+    expect(createdFlashcard.answer).to.be.equal('An easier way to manage state');
+    expect(createdFlashcard.type).to.be.equal('redux');
     })
   }) // end describe('/api/users')
 }) // end describe('User routes')
