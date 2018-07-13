536fa26f7bea93f2bd4545d19e05655648621c91
jmunoz1992 <jasmine.esplago.munoz@gmail.com>
2018-07-13 08:37:17 -0500

    flashcard can be editted and deleted

--- server/api/flashcards.js
+++ server/api/flashcards.js
@@ -30,3 +30,35 @@ router.post('/', async (req, res, next) => {
     next(err)
   }
 })
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    await Flashcard.update({
+      question: req.body.question,
+      answer: req.body.answer,
+      type: req.body.type,
+    }, {
+      where: {
+        id: req.params.id
+      }
+    })
+    const updatedFlashcard = await Flashcard.findById(req.params.id);
+    console.log('updated flashcard ', updatedFlashcard);
+    res.status(201).json(updatedFlashcard)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    await Flashcard.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    res.send('this flashcard has been destroyed')
+  } catch (err) {
+    next(err)
+  }
+})
