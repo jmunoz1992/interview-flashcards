beef310fcb56fefecb393aac5c5c56bed3977b85
jmunoz1992 <jasmine.esplago.munoz@gmail.com>
2018-09-13 20:02:45 -0500

    states total flashcards finished in nav bar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -2,52 +2,59 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
-import {logout} from '../store'
+import {logout, fetchFlashcards} from '../store'
 
-const Navbar = ({handleClick, isLoggedIn, user}) => {
-  const leftNav = {
-    flexGrow: 1,
-    display: "flex"
+class Navbar extends React.Component {
+  componentDidMount() {
+    this.props.getFlashcards();
   }
-  const rightNav = {
-    flexGrow: 1,
-    display: "flex",
-    justifyContent: "flex-end"
-  }
-  const h3Style = {
-    margin: '0.5em 0.75em'
-  }
-  const homeLogoutStyle = {
-    margin: '0.5em 0.75em',
-    color: 'blue'
-  }
-  return (
-    <div>
-      <nav>
-      {isLoggedIn ? (
-        <div id="full-nav">
-          <div style={leftNav}>
-            <Link style={homeLogoutStyle} to="/">Home</Link>
+  render() {
+    const {handleClick, isLoggedIn, user, flashcards} = this.props
+    console.log('user points ', user.totalPoints)
+    const leftNav = {
+      flexGrow: 1,
+      display: "flex"
+    }
+    const rightNav = {
+      flexGrow: 1,
+      display: "flex",
+      justifyContent: "flex-end"
+    }
+    const h3Style = {
+      margin: '0.5em 0.75em'
+    }
+    const homeLogoutStyle = {
+      margin: '0.5em 0.75em',
+      color: 'blue'
+    }
+    return (
+      <div>
+        <nav>
+        {isLoggedIn ? (
+          <div id="full-nav">
+            <div style={leftNav}>
+              <Link style={homeLogoutStyle} to="/">Home</Link>
+            </div>
+            <div style={rightNav}>
+              <h3 style={h3Style}>Welcome {user.email}</h3>
+              <h3 style={h3Style}>Completed {user.totalPoints ? user.totalPoints : 0} out of {flashcards.length} flashcards</h3>
+              <a style={homeLogoutStyle} onClick={handleClick}>
+                Logout
+              </a>
+            </div>
           </div>
-          <div style={rightNav}>
-            <h3 style={h3Style}>Welcome {user.email}</h3>
-            <h3 style={h3Style}>Total Points: {user.totalPoints}</h3>
-            <a style={homeLogoutStyle} onClick={handleClick}>
-              Logout
-            </a>
+        ) : (
+          <div>
+            {/* The navbar will show these links before you log in */}
+            <Link to="/">Home</Link>
+            <Link to="/login">Login</Link>
           </div>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/">Home</Link>
-          <Link to="/login">Login</Link>
-        </div>
-      )}
-    </nav>
-    <hr />
-  </div>
-  )
+        )}
+      </nav>
+      <hr />
+    </div>
+    )
+  }
 }
 
 /**
@@ -57,6 +64,7 @@ const mapState = state => {
   return {
     isLoggedIn: !!state.user.id,
     user: state.user,
+    flashcards: state.flashcards
   }
 }
 
@@ -64,7 +72,10 @@ const mapDispatch = dispatch => {
   return {
     handleClick() {
       dispatch(logout())
-    }
+    },
+    getFlashcards() {
+      dispatch(fetchFlashcards())
+    },
   }
 }
 
